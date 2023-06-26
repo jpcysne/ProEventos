00307a4d7434592b639960d82224fc83cf4af238
João Rubio <jrubio@alter-solutions.com>
2023-06-26 17:04:13 +0200

    Modificaçoes na tela de eventos

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,11 +9,36 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit{
 
   public eventos: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList(){
+    return this._filterList;
+  }
+
+  public set filterList(value: string){
+    this._filterList = value;
+    this.eventos = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+  }
+
 
   constructor(private http: HttpClient){}
 
   ngOnInit(): void {
     this.getEventos();
+  }
+
+  changeImage(){
+    this.showImg = !this.showImg;
+  }
+
+  filterEvents(filterBy: string): any {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.eventos.filter(
+        (evento: { tema: string; local: string;}) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1
+      );
   }
 
   public getEventos(): void {
