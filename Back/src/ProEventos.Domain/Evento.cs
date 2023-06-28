625abdabaa814b2ff525415add8680aed2401649
jpcysne <joaopaulorubio@hotmail.com>
2023-06-28 20:14:52 +0200

    Modificaçao no Back

--- Back/src/ProEventos.Domain/Evento.cs
+++ Back/src/ProEventos.Domain/Evento.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProEventos.API.Models
+namespace ProEventos.Domain
 {
     public class Evento
     {
-        public int EventoId {get; set;}
+        public int Id {get; set;}
         public string Local {get;set;}
-        public string DataEvento{get; set;}
+        public DateTime? DataEvento{get; set;}
         public string Tema {get; set;}
         public int QtdPessoas{get; set;}
         public string Lote {get; set;}
         public string ImagemURL {get; set;}
+        public string Telefone {get; set;}
+        public string Email { get; set; }
+        public IEnumerable<Lote> Lote { get; set; }
+        public IEnumerable<RedeSocial> RedeSociais { get; set; }
     }
 }
